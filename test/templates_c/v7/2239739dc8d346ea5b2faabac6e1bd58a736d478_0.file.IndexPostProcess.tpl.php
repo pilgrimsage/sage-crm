7e932d12844406ef159463f3b6f9f96ed677c0f7
pilgrimsage <sksmukhia.dev@gmail.com>
2026-08-30 00:38:42 +0530

    Initiate Project

---
+++ test/templates_c/v7/2239739dc8d346ea5b2faabac6e1bd58a736d478_0.file.IndexPostProcess.tpl.php
@@ -0,0 +1,28 @@
+<?php
+/* Smarty version 4.5.5, created on 2026-08-29 18:57:52
+  from '/Applications/XAMPP/xamppfiles/htdocs/vtigercrm/layouts/v7/modules/Vtiger/IndexPostProcess.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.5',
+  'unifunc' => 'content_6a932bb03a8423_57047801',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '2239739dc8d346ea5b2faabac6e1bd58a736d478' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/vtigercrm/layouts/v7/modules/Vtiger/IndexPostProcess.tpl',
+      1 => 1752039682,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6a932bb03a8423_57047801 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+	</div>
+</div>
+</div><?php }
+}
